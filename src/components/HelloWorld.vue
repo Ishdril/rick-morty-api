7fbe26d43a9437e85394ed5bfb03010ffcd45067
Ishdril <berni2015@gmail.com>
2021-05-28 00:14:02 +0200

    feat: vue setup and boilerplate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -138,19 +138,18 @@ export default defineComponent({
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
+<style scoped lang="sass">
+h3
   margin: 40px 0 0;
-}
-ul {
+
+ul
   list-style-type: none;
   padding: 0;
-}
-li {
+
+li
   display: inline-block;
   margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+a
+  color: #ff0000;
 </style>
